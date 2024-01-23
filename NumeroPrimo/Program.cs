3f9da718e685dfd273cca6e74514d3ac2d9150cf
babingthon <babingthon.bandeira@gmail.com>
2024-01-23 16:03:14 -0300

    verificar se numero é primo

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,18 @@
+﻿int numeroTeste;
+int divisoresTeste = 0;
+
+Console.WriteLine("Informe o número a ser testado: ");
+numeroTeste = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=numeroTeste; i++)
+{
+    if (numeroTeste % i == 0)
+        divisoresTeste++;
+}
+
+if (divisoresTeste == 2)
+    Console.WriteLine("É número primo.");
+else
+    Console.WriteLine("É número par.");
+
+Console.ReadKey();
